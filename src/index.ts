e52bfc10bf2bccc69d30aa1f72e5fee652a2fee8
Wesley Scholl <wscholl@totalwine.com>
2023-09-06 12:38:05 -0400

    Added required inputs for discussion comments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import { getInput } from '@actions/core';
+
+const token = getInput('token');
+const graphqlUrl = getInput('graphql-url');
+const body = getInput('body');
+const discussionId = getInput('discussion-id');
+const gqlClientMutationId = getInput('gql-client-mutation-id');
+    
+console.log(`Token: ${token}!`);
+console.log(`GraphQL URL: ${graphqlUrl}!`);
+console.log(`Comment Body: ${body}!`);
+console.log(`Discussion Topic ID: ${discussionId}!`);
+console.log(`GraphQL Client Mutation ID: ${gqlClientMutationId}!`);
